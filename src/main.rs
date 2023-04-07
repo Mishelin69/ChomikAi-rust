21764aaec013498a7b4a260110eb055f6eaf2718
Mishelin69 <miloskosec90@gmail.com>
2023-04-07 22:56:33 +0200

    big performance improvements, crazy like a panda

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,19 @@ pub mod func;
 
 fn main() {
 
-    let mut network = Network::new(&[2, 2, 1]);
+    let mut network = Network::new(&[2, 200, 200, 1]);
     network.init_self();
 
     let mut inp = vec![1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0];
-    let mut expc = vec![1.0, 0.0, 0.0, 0.0];
+    let mut expc = vec![0.0, 0.0, 1.0, 1.0];
 
     network.learn(&mut inp, &mut expc, 10000, 0.1);
 
-    let mut out: Vec<Vec<f64>> = network.helper_init_actv(false);
+    let mut out: Vec<f64> = network.helper_init_actv(false);
 
     for i in 0..4_usize {
 
-        network.feedforward(&inp[i*network.shape_in..(1+i)*network.shape_in], &mut out, false);
+        network.feedforward(&inp[i*network.shape_in..(1+i)*network.shape_in], &mut out);
 
         println!("EXP: {:?}", expc);
         println!("IN: {:?}", inp);
--- src/network.rs
+++ src/network.rs
@@ -2,6 +2,29 @@ use rand::{rngs::ThreadRng, Rng, thread_rng};
 
 use crate::layer::Layer;
 
+/*
+<========================================================================================================>
+TODO: 
+  -> get rid of 2D arrays => reduce cache misses ✓
+  -> rewrite `calc_delta` to work with the new system ✓
+  -> try to implement multithreaded front pass
+  -> implement multithreaded  learning
+  -> maybe improve performance somewhere else ✓ (by half actually [>=])
+<========================================================================================================>
+*/
+
+pub fn make_flat_copy(v: &[Vec<f64>], expc_s: usize) -> Vec<f64> {
+
+    let mut cpy = Vec::with_capacity(expc_s);
+
+    for x in v {
+        for e in x {
+            cpy.push(*e);
+        }
+    }
+    cpy
+}
+
 pub struct Network<'a> {
 
     layers: Vec<Layer>,
@@ -42,112 +65,138 @@ impl<'a> Network<'a> {
 
     }
 
-    pub fn feedforward(&self, input: &[f64], out: &mut Vec<Vec<f64>>, rev: bool) {
+    pub fn feedforward(&self, input: &[f64], out: &mut Vec<f64>) {
 
         let elm: usize = input.len() / self.shape_in;
         let mut off: usize = 0;
-        let mut test_off: usize = 0;
+        // let mut test_off: usize = 0;
 
         for _ in 0..elm {
 
             let mut layers = self.layers.iter();
-            let last_layer = layers.next().unwrap();
+            let mut last_layer = layers.next().unwrap();
 
             for i in 0_usize..last_layer.ndc {
 
-                out[off][i] = last_layer.bias[i];
+                // out[off][i] = last_layer.bias[i];
+                out[i] = last_layer.bias[i];
                 for j in 0_usize..last_layer.n {
-                    out[off][i] += input[j] * last_layer.nodes[j].w[i];
+                    // out[off][i] += input[j] * last_layer.nodes[j].w[i];
+                    out[off+i] += input[j] * last_layer.nodes[j].w[i];
                 }
 
-                out[off][i] = last_layer.run_actv(out[off][i]);
+                out[off+i] = last_layer.run_actv(out[off+i]);
+                // out[off][i] = last_layer.run_actv(out[off][i]);
             }
 
-            test_off += last_layer.ndc;
-            off += 1;
+            off += last_layer.ndc;
+            // off += 1;
 
             while let Some(layer) = layers.next() {
 
                 for i in 0_usize..layer.ndc {
 
-                    out[off][i] = layer.bias[i];
+                    // out[off][i] = layer.bias[i];
+                    out[off+i] = layer.bias[i];
                     for j in 0_usize..layer.n {
-                        out[off][i] += out[off-1][j] * layer.nodes[j].w[i];
+                        // out[off][i] += out[off-1][j] * layer.nodes[j].w[i];
+                        out[off+i] += out[off-last_layer.ndc+j] * layer.nodes[j].w[i];
                     }
 
-                    out[off][i] = layer.run_actv(out[off][i]);
+                    out[off+i] = layer.run_actv(out[off+i]);
+                    // out[off][i] = layer.run_actv(out[off][i]);
                 }
 
-                off += 1;
+                off += layer.ndc;
+                // off += 1;
+                last_layer = &layer;
             }
         }
+        // println!("OUT: {:?}", out);
 
-        if rev {
-            println!("{:?}", out);
-            out.reverse();
-            println!("{:?}", out);
-        }
+        // if rev {
+        //     out.reverse();
+        // }
     }
 
-    fn calc_delta(&self, actv: &[Vec<f64>], crt: &[f64], out: &mut Vec<Vec<f64>>) {
+    fn calc_delta(&self, actv: &Vec<f64>, crt: &[f64], out: &mut Vec<f64>) {
 
-        let mut off: usize = 0;
+        // let mut off: usize = 0;
         let mut layers_reversed = self.layers.iter().rev();
         let mut last_layer = layers_reversed.next().unwrap();
+        // let mut test_out = Vec::<f64>::with_capacity(self.nodes_total); unsafe { test_out.set_len(self.nodes_total); }
+        let mut off: usize = self.nodes_total - last_layer.ndc;
+        let mut test_start = 0;
 
         //calc error in output layer 
         for i in 0..last_layer.ndc {
-            out[off][i] = (crt[i] - actv[off][i]) * last_layer.run_der(actv[off][i]);
+            // out[off][i] = (crt[i] - actv[off][i]) * last_layer.run_der(actv[off][i]);
+            out[test_start] = (crt[i] - actv[off + i]) * last_layer.run_der(actv[off + i]);
         }
+        test_start += last_layer.ndc;
 
         let mut last_layer_nodes = last_layer.ndc;
-        off += 1;
+        // off += 1;
 
         //calc error in the rest, stop at input 
         while let Some(layer) = layers_reversed.next() {
 
+            off -= layer.ndc;
             for i in 0..layer.ndc {
 
                 let mut err: f64 = 0.0;
+                // let mut test_err: f64 = 0.0;
                 for j in 0..last_layer_nodes {
-                    err += out[off-1][j] * last_layer.nodes[i].w[j];
+                    // err += out[off-1][j] * last_layer.nodes[i].w[j];
+                    err += out[test_start - i - last_layer.ndc + j] * last_layer.nodes[i].w[j];
                 }
 
-                out[off][i] = err * layer.run_der(actv[off][i]);
+                // println!("ERR CMP {} {}", err, test_err);
+                // out[off][i] = err * layer.run_der(actv[off][i]);
+                out[test_start] = err * layer.run_der(actv[off + i]);
+                test_start += 1;
             }
 
-            off += 1;
+            // off += 1;
             last_layer_nodes = layer.ndc;
             last_layer = layer;
         }
+        // println!("CMPR:\n{:?}\n{:?}", make_flat_copy(out, self.nodes_total), test_out);
     }
 
-    fn apply_change(&mut self, input: &[f64], actv: &[Vec<f64>], dlt: &Vec<Vec<f64>>, lr: f64) {
+    fn apply_change(&mut self, input: &[f64], actv: &Vec<f64>, dlt: &Vec<f64>, lr: f64) {
 
         let mut layer_ref = self.layers.iter_mut().rev();
-        let mut off: usize = 0;
+        let mut off: usize = self.nodes_total;
+        let mut dlt_off = 0;
 
         for _ in 0..self.layers_total-1 {
 
             let layer = layer_ref.next().unwrap();
+            off -= layer.ndc;
+
             for i in 0..layer.ndc {
 
-                layer.bias[i] += dlt[off][i] * lr;
+                // layer.bias[i] += dlt[off][i] * lr;
+                layer.bias[i] += dlt[dlt_off + i] * lr;
                 for j in 0..layer.n {
-                    layer.nodes[j].w[i] += actv[off+1][j] * dlt[off][i] * lr;
+                    // layer.nodes[j].w[i] += actv[off+1][j] * dlt[off][i] * lr;
+                    layer.nodes[j].w[i] += actv[off - layer.n + j] * dlt[dlt_off + i] * lr;
                 }
             }
 
-            off += 1;
+            dlt_off += layer.ndc;
         }
 
         let last_layer = layer_ref.next().unwrap();
 
         for i in 0..last_layer.ndc  {
 
-            last_layer.bias[i] += dlt[off][i] * lr;
+            // last_layer.bias[i] += dlt[off][i] * lr;
+            last_layer.bias[i] += dlt[dlt_off + i] * lr;
             for j in 0..last_layer.n {
-                last_layer.nodes[j].w[i] += input[j] * dlt[off][i] * lr;
+                // last_layer.nodes[j].w[i] += input[j] * dlt[off][i] * lr;
+                last_layer.nodes[j].w[i] += input[j] * dlt[dlt_off + i] * lr;
             }
         }
     }
@@ -156,15 +205,18 @@ impl<'a> Network<'a> {
 
         let elm_am: usize = correct.len() / self.shape_out;
         let mut rng = thread_rng();
-        let mut cur_pred: Vec<Vec<f64>> = self.helper_init_actv(false);
-        let mut delta: Vec<Vec<f64>> = self.helper_init_actv(true); 
+        let mut cur_pred: Vec<f64> = self.helper_init_actv(false);
+        let mut delta: Vec<f64> = self.helper_init_actv(false); 
 
         for _ in 0..n_epochs {
             for i in 0..elm_am {
 
                 //shuffle, calculate delta, apply change, repeat
                 self.helper_shuffle_in(inp, correct, &mut rng);
-                self.feedforward(&inp[(i*self.shape_in)..((i+1)*self.shape_in)], &mut cur_pred, true);
+                self.feedforward(
+                    &inp[(i*self.shape_in)..((i+1)*self.shape_in)], 
+                    &mut cur_pred,
+                );
 
                 self.calc_delta(
                     &cur_pred, 
@@ -172,32 +224,32 @@ impl<'a> Network<'a> {
                     &mut delta,
                     );
 
-                self.apply_change(&inp[(i*self.shape_in)..((i+1)*self.shape_in)], &cur_pred, &delta, lr);
+                self.apply_change(
+                    &inp[(i*self.shape_in)..((i+1)*self.shape_in)], 
+                    &cur_pred, 
+                    &delta, 
+                    lr,
+                );
 
-                cur_pred.reverse();
+                // cur_pred.reverse();
             }
         }
         println!("EXIT!!!!");
     }
 
-    pub fn helper_init_actv(&self, rev: bool) -> Vec<Vec<f64>> {
+    pub fn helper_init_actv(&self, rev: bool) -> Vec<f64> {
 
-        let mut v = Vec::with_capacity(self.layers_total);
-        let mut i = 0;
-
-        for l in &self.layers {
-
-            v.push(Vec::with_capacity(l.ndc));
-            unsafe { v[i].set_len(l.ndc);}
-            i += 1_usize;
-
-        }
+        let mut v = Vec::with_capacity(self.nodes_total);
+        unsafe { v.set_len(self.nodes_total ); }
 
         if rev {
             v.reverse()
         }
 
         v
+    }
+
+    pub fn multthrd_learn(&mut self, max_workers: usize, train_data: Vec<f64>, correct: Vec<f64>, epochs: usize, lr: f64) {
     }
 
     fn helper_shuffle_in(&self, arg1: &mut Vec<f64>, arg2: &mut Vec<f64>, rng: &mut ThreadRng) {
